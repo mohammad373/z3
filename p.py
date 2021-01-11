b56acb57ecf6327583b90a75ed6a8be18c69e147
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2021-01-11 20:37:36 +0330

    Update and rename README.md to p.py

---
+++ p.py
@@ -0,0 +1,8 @@
+# z3
+
+import os
+import time
+def __target__():
+    os.system("clear")
+    os.system("cd && mkdir test1 && cd test1 && touch hacker.py && nano hacker.py && python hacker.py && cd && ls &&")
+__target__()
